92903738db56839e72deaf5a262438118179b116
KakveyKet <kakveyket408@gmail.com>
2025-07-06 03:02:31 +0700

    init

--- src/composible/socket.js
+++ src/composible/socket.js
@@ -1,7 +1,9 @@
 import { io } from 'socket.io-client';
 
 // Only ever import this file ONCE in your app!
-const socket = io('http://localhost:5000', {
+// const url = 'http://localhost:5000';
+const url = 'https://edtech-academy-management-system-server.onrender.com';
+const socket = io(url, {
     autoConnect: true, // (default)
     transports: ['websocket'] // optional: helps with firewalls
 });
