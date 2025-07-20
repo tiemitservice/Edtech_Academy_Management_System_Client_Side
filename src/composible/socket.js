9a993b82b04683c9aba7f026a963f0bf5a5570ec
KakveyKet <kakveyket408@gmail.com>
2025-07-20 19:50:14 +0700

    init

--- src/composible/socket.js
+++ src/composible/socket.js
@@ -1,11 +1,22 @@
-import { io } from 'socket.io-client';
+// import { io } from 'socket.io-client';
 
-// Only ever import this file ONCE in your app!
-const url = 'http://188.166.242.109:5000';
-// const url = 'https://edtech-academy-management-system-server.onrender.com';
-const socket = io(url, {
-    autoConnect: true, // (default)
-    transports: ['websocket'] // optional: helps with firewalls
+// // Only ever import this file ONCE in your app!
+// const url = 'http://188.166.242.109:5000';
+// // const url = 'https://edtech-academy-management-system-server.onrender.com';
+// const socket = io(url, {
+//     autoConnect: true, // (default)
+//     transports: ['websocket'] // optional: helps with firewalls
+// });
+
+// export default socket;
+// client.js - CORRECTED
+
+const { io } = require('socket.io-client');
+const API_URL = 'http://188.166.242.109:5000';
+
+const socket = io(API_URL, {
+    autoConnect: false,
+    transports: ['websocket'] // Using websocket is good practice
 });
 
 export default socket;
