09ab050b91ae28c69c7634abbca9029170d48d4e
KakveyKet <kakveyket408@gmail.com>
2025-07-21 08:51:00 +0700

    init

--- src/composible/api.js
+++ src/composible/api.js
@@ -1,4 +1,4 @@
-const url = 'http://localhost:5000';
+// const url = 'http://localhost:5000';
 
-// const url = 'http://188.166.242.109:5000';
+const url = 'http://188.166.242.109:5000';
 export default url;
--- src/composible/socket.js
+++ src/composible/socket.js
@@ -1,8 +1,8 @@
 import { io } from 'socket.io-client';
 
 // Only ever import this file ONCE in your app!
-const url = 'http://localhost:5000';
-// const url = 'http://188.166.242.109:5000';
+// const url = 'http://localhost:5000';
+const url = 'http://188.166.242.109:5000';
 const socket = io(url, {
     autoConnect: true, // (default)
     transports: ['websocket'] // optional: helps with firewalls
